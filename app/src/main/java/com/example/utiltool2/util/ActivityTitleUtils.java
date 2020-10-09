2f81683d8c0d106a38075862627e42a387526c63
lgh <ysulgh@163.com>
2020-10-09 18:09:26 +0800

    contentprovider+sharepreferences、activity.getTitle

---
+++ app/src/main/java/com/example/utiltool2/util/ActivityTitleUtils.java
@@ -0,0 +1,81 @@
+package com.example.utiltool2.util;
+
+import android.annotation.TargetApi;
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.text.TextUtils;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @author lgh on 2020/10/9:16:37
+ * @description
+ */
+public class ActivityTitleUtils {
+
+    /**
+     * 获取 Activity 的 title
+     *
+     * @param activity Activity
+     * @return
+     */
+    public static String getActivityTitle(Activity activity) {
+        String activityTitle = null;
+
+        if (activity == null) {
+            return null;
+        }
+
+        try {
+            activityTitle = activity.getTitle().toString();
+
+            if (Build.VERSION.SDK_INT >= 11) {
+                String toolbarTitle = getToolbarTitle(activity);
+                if (!TextUtils.isEmpty(toolbarTitle)) {
+                    activityTitle = toolbarTitle;
+                }
+            }
+
+            if (TextUtils.isEmpty(activityTitle)) {
+                PackageManager packageManager = activity.getPackageManager();
+                if (packageManager != null) {
+                    ActivityInfo activityInfo = packageManager.getActivityInfo(activity.getComponentName(), 0);
+                    if (activityInfo != null) {
+                        activityTitle = activityInfo.loadLabel(packageManager).toString();
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return activityTitle;
+    }
+
+    @TargetApi(11)
+    private static String getToolbarTitle(Activity activity) {
+        try {
+            ActionBar actionBar = activity.getActionBar();
+            if (actionBar != null) {
+                if (!TextUtils.isEmpty(actionBar.getTitle())) {
+                    return actionBar.getTitle().toString();
+                }
+            } else {
+                if (activity instanceof AppCompatActivity) {
+                    AppCompatActivity appCompatActivity = (AppCompatActivity) activity;
+                    androidx.appcompat.app.ActionBar supportActionBar = appCompatActivity.getSupportActionBar();
+                    if (supportActionBar != null) {
+                        if (!TextUtils.isEmpty(supportActionBar.getTitle())) {
+                            return supportActionBar.getTitle().toString();
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
